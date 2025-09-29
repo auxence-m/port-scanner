f76c284d84ff96be2be2f98f5c0e3e093f3f0ef0
Auxence-M <auxencemedja@gmail.com>
2025-09-29 17:09:14 -0400

    Allow the user to provide port ranges, such as 1-1024, in addition to specific ports for scan

--- cmd/scan.go
+++ cmd/scan.go
@@ -5,22 +5,46 @@ import (
 	"io"
 	"os"
 	"pScan/scan"
+	"strconv"
+	"strings"
 
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 )
 
 var scannedPorts []int
+var portRange string
 
 // scanCmd represents the scan command
 var scanCmd = &cobra.Command{
-	Use:   "scan",
-	Short: "Run a port scan on the hosts",
-	RunE:  scanRun,
+	Use:          "scan",
+	Short:        "Run a port scan on the hosts",
+	SilenceUsage: true,
+	RunE:         scanRun,
 }
 
 func scanRun(cmd *cobra.Command, args []string) error {
 	hostsFile := viper.GetString("hosts-file")
+
+	if portRange != "" {
+		rangeStr := strings.Split(portRange, "-")
+		if len(rangeStr) != 2 {
+			return fmt.Errorf("parsing \"%s\": invalid port range format", portRange)
+		}
+
+		start, err := strconv.Atoi(rangeStr[0])
+		if err != nil {
+			return err
+		}
+		end, err := strconv.Atoi(rangeStr[1])
+		if err != nil {
+			return err
+		}
+
+		for i := start; i <= end; i++ {
+			scannedPorts = append(scannedPorts, i)
+		}
+	}
 
 	return scanAction(os.Stdout, hostsFile, scannedPorts)
 }
@@ -64,4 +88,5 @@ func printResults(out io.Writer, results []scan.Results) error {
 func init() {
 	rootCmd.AddCommand(scanCmd)
 	scanCmd.Flags().IntSliceVarP(&scannedPorts, "ports", "p", []int{22, 80, 443}, "ports to scan")
+	scanCmd.Flags().StringVarP(&portRange, "range", "r", "", "port range to scan")
 }
