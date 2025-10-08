7dc03ee58aa67e67c68e1d98be7301024dd62bba
Auxence-M <auxencemedja@gmail.com>
2025-10-08 15:35:38 -0400

    Added Charm/fang for nicer outputs

--- main.go
+++ main.go
@@ -1,18 +1,25 @@
 package main
 
 import (
+	"context"
 	"log"
+	"os"
 	"pScan/cmd"
 
+	"github.com/charmbracelet/fang"
 	"github.com/spf13/cobra/doc"
 )
 
 func main() {
-	cmd.Execute()
+	//cmd.Execute()
 
 	rootCmd := cmd.Root()
 	err := doc.GenMarkdownTree(rootCmd, "./docs")
 	if err != nil {
 		log.Fatal(err)
+	}
+
+	if err := fang.Execute(context.Background(), rootCmd); err != nil {
+		os.Exit(1)
 	}
 }
